bfef6a3f31ea198e7783d20aea9ae258a1eb15f5
Bunyameen <bunyameen@gmail.com>
2023-02-18 09:21:37 +0100

    feat: added Feedbackstats components

--- src/Components/FeedbackStas.jsx
+++ src/Components/FeedbackStas.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types'
+
 
 const FeedbackStas = ({feedback}) => {
+    let average = feedback.reduce((acc, cur) => {
+        return acc + cur.rating
+
+    } , 0) / feedback.length
+
+    average = average.toFixed(1).replace(/[.,]0$/, '')
     return (
         <div className='feedback-stats'>
             <h4>{feedback.length} Reviews</h4>
-            <h4>Average Rating</h4>
+            <h4>{isNaN(average) ? 0 : average} Average Rating</h4>
         </div>
     );
+}
+
+FeedbackStas.propTypes = {
+    FeedbackStas: PropTypes.array.isRequired
 }
 
 export default FeedbackStas;
