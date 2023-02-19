38d04e9c3dfe20fd5f8066429dacc8ff191c74d3
Bunyameen <bunyameen@gmail.com>
2023-02-19 20:00:59 +0100

    feat: completed Project: Feedback Reviews Ui

--- src/Components/FeedbackList.jsx
+++ src/Components/FeedbackList.jsx
@@ -41,20 +41,7 @@ function FeedbackList() {
     
     </div>
   )
-//   return (
-//     <div className='feedback-list'>
-//         {
-//             feedback.map((item) => {
-//                 return(
-//                    <FeedbackItem key={item.id} item ={item}
-//                    handleDelete = {handleDelete}
-//                     />
-//                 )
-//             })
-//         }
-    
-//     </div>
-//   )
+
 }
 
 
